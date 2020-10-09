7276c096dc7af07c3a518a7b397fe551d154a0a1
Nicolas Gelders <Nicolas.Gelders@vrt.be>
2020-10-09 15:11:33 +0200

    added ecs task def

--- ecs.tf
+++ ecs.tf
@@ -12,3 +12,39 @@ resource "aws_ecs_cluster" "airflow" {
   }
 }
 
+resource "aws_ecs_task_definition" "airflow" {
+  family                   = "airflow"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.ecs_cpu
+  memory                   = var.ecs_memory
+  network_mode             = "awsvpc"
+  task_role_arn            = aws_iam_role.task.arn
+  execution_role_arn       = aws_iam_role.execution.arn
+  container_definitions    = <<TASK_DEFINITION
+  [
+    {
+        "image": "${var.airflow_image_name}:${var.airflow_image_tag}",
+        "cpu" : ${var.ecs_cpu},
+        "memory": ${var.ecs_memory},
+        "name": "airflow",
+        "environment": [
+            {"name": "AIRFLOW__WEBSERVER__NAVBAR_COLOR", "value": "${var.airflow_navbar_color}"},
+        "logConfiguration": {
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-group": "${aws_cloudwatch_log_group.airflow.name}",
+            "awslogs-region": "eu-west-1",
+            "awslogs-stream-prefix": "container"
+          }
+        },
+        "essential": true,
+        "portMappings": [
+            {
+                "containerPort": 8080,
+                "hostPort": 8080
+            }
+        ]
+    }
+  ]
+  TASK_DEFINITION
+}
