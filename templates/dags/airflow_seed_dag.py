ebb2a24dcef2df579e9ec6e7f811198b693964c1
Nicolas Gelders <Nicolas.Gelders@vrt.be>
2020-10-23 10:08:59 +0200

    cleaned up seed dag

--- templates/dags/airflow_seed_dag.py
+++ templates/dags/airflow_seed_dag.py
@@ -1,20 +1,15 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
 import datetime
 from typing import Dict
-from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
-from airflow import AirflowException
-
-from airflow.api.common.experimental.delete_dag import delete_dag
 
 import boto3
-import re
-from os import listdir
-import os
-from os.path import isfile, join
 
 # The name of this file
 seed_dag_file_name = os.path.basename(__file__)
@@ -27,11 +22,12 @@ s3_key_dags = "dags"
 folder_local = "${AIRFLOW_HOME}/dags"
 
 args = {
-    "start_date": datetime.datetime(2020, 10, 16),
+    "start_date": datetime.datetime(${YEAR}, ${MONTH}, ${DAY}),
 }
 
+# we prefix the dag with '0' to make it the first dag
 with DAG(
-    dag_id="_sync_dags_in_s3_to_local_airflow_dags_folder",
+    dag_id="0_sync_dags_in_s3_to_local_airflow_dags_folder",
     default_args=args,
     schedule_interval=None
 ) as dag:
@@ -39,26 +35,6 @@ with DAG(
         task_id="list_dags_before",
         bash_command="ls ${AIRFLOW_HOME}/dags",
     )
-
-    def get_dag_id(dag_file_path: str) -> str:
-        """Gets the dag id in a file."""
-        with open(dag_file_path, "r") as file:
-            all_python_code = file.read()
-
-        dag_id_search = re.search(
-            'dag_id="(.*)",.*',
-            all_python_code,
-            re.IGNORECASE
-        )
-
-        dag_id = None
-        if dag_id_search:
-            dag_id = dag_id_search.group(1)
-
-        if dag_id == None:
-            raise Exception("No dag_id found")
-
-        return dag_id
 
     def sync_s3_dags_to_local_dags(**context: Dict) -> None:
         """Syncs an S3 bucket with a local folder in the airflow container."""
@@ -74,17 +50,13 @@ with DAG(
             bucket.download_file(object.key, f"{folder_local}/{file_name}")
 
         # remove all the files and dags not in s3 from the local folder
-        # also delete the dag
         files_in_dag_folder = [f for f in listdir(folder_local) if isfile(join(folder_local, f))]
 
         for file_name in files_in_dag_folder:
             dag_file_path = f"{folder_local}/{file_name}"
             if file_name not in dag_files_in_s3:
-                dag_id = get_dag_id(dag_file_path)
                 print(f"Removing dag file: {folder_local}/{file_name}")
                 os.remove(f"{folder_local}/{file_name}")
-                print(f"Removing dag with id: {dag_id}")
-                delete_dag(dag_id)
 
     sync_dags = PythonOperator(
         task_id="sync_dags_in_s3_to_local_dags",
