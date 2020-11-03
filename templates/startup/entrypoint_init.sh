0b50e1a5a48c9f030b5ca0f56ebee42d8faf25a5
Nicolas Gelders <Nicolas.Gelders@vrt.be>
2020-11-03 21:15:58 +0100

    dont create users if there are users in db

--- templates/startup/entrypoint_init.sh
+++ templates/startup/entrypoint_init.sh
@@ -4,6 +4,11 @@ airflow initdb
 # add admin user if not exists
 if [ "${RBAC_AUTH}" == "true" ]; then
     # todo if no users add admin user
-    airflow list_users
-    airflow create_user -r Admin -u admin -e admin@admin.com -f Admin -l airflow -p admin
+    amount_of_users=$(python -c 'import sys;print((sys.argv.count("│") // 7) - 1)' $(airflow list_users))
+    if [ "$amount_of_users" == "0" ]; then
+        echo "Adding admin users, users list is empty!"
+        airflow create_user -r Admin -u admin -e admin@admin.com -f Admin -l airflow -p admin
+    else
+        echo "No admin user added, users already exists!"
+    fi
 fi
