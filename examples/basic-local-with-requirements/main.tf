405a66c443af20991cab81cba0cd066f5f0d8445
dpf <dpf>
2021-08-18 10:29:11 +0200

    Run terraform fmt

--- examples/basic-local-with-requirements/main.tf
+++ examples/basic-local-with-requirements/main.tf
@@ -12,26 +12,26 @@ provider "aws" {
 }
 
 module "airflow" {
-    source = "datarootsio/ecs-airflow/aws"
+  source = "datarootsio/ecs-airflow/aws"
 
-    resource_prefix = "my-awesome-company"
-    resource_suffix = "env"
+  resource_prefix = "my-awesome-company"
+  resource_suffix = "env"
 
-    vpc_id             = "vpc-123456"
-    public_subnet_ids  = ["subnet-456789", "subnet-098765"]
+  vpc_id            = "vpc-123456"
+  public_subnet_ids = ["subnet-456789", "subnet-098765"]
 
-    use_https = false
+  use_https = false
 
-    airflow_executor = "Local"
-    airflow_py_requirements_path = "./requirements.txt"
-    airflow_variables = {
-      AIRFLOW__WEBSERVER__NAVBAR_COLOR : "#e27d60"
-    }
+  airflow_executor             = "Local"
+  airflow_py_requirements_path = "./requirements.txt"
+  airflow_variables = {
+    AIRFLOW__WEBSERVER__NAVBAR_COLOR : "#e27d60"
+  }
 
-    rds_username            = "airflow"
-    rds_password            = "super-secret-pass"
-    rds_availability_zone   = "eu-west-1a"
-    rds_deletion_protection = false
+  rds_username            = "airflow"
+  rds_password            = "super-secret-pass"
+  rds_availability_zone   = "eu-west-1a"
+  rds_deletion_protection = false
 }
 
 output "airflow_alb_dns" {
