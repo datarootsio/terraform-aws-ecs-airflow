927cf090c712c79579aaa5b53dabc5be4d5e3b8c
Milena Angelova <milena.angelova@eldersoss.com>
2021-12-10 13:36:15 +0200

    changed the ecs task definition

--- ecs.tf
+++ ecs.tf
@@ -183,9 +183,6 @@ resource "aws_ecs_task_definition" "airflow" {
       {
         "image": "mikesir87/aws-cli",
         "name": "${local.airflow_dags_sync_container_name}",
-        "command": [
-            "/bin/bash -c \"aws s3 sync --exclude='*' --include='*.py' --size-only --delete s3://${local.s3_bucket_name}/dags/ ${var.airflow_container_home}/dags/\""
-        ],
         "entryPoint": [
             "sh",
             "-c"
