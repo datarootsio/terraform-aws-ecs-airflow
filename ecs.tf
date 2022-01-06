4971937258af561a224aeabed4c0a6aff6cacbda
Milena Angelova <milena.angelova@eldersoss.com>
2022-01-06 16:36:03 +0200

    changed s3 location arn

--- ecs.tf
+++ ecs.tf
@@ -281,7 +281,7 @@ resource "aws_efs_mount_target" "airflow" {
 }
 
 resource "aws_datasync_location_s3" "s3_location" {
-  s3_bucket_arn = "${local.s3_bucket_name.arn}"
+  s3_bucket_arn = "${local.s3_bucket_source_arn}"
   subdirectory  = "${var.datasync_location_s3_subdirectory}"
 
   s3_config {
