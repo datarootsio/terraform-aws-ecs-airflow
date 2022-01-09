892a717112934a151a5929d8e99cd2f40587d503
Milena Angelova <milena.angelova@eldersoss.com>
2022-01-09 21:09:38 +0200

    added a lambda function

--- s3.tf
+++ s3.tf
@@ -96,3 +96,37 @@ resource "aws_s3_bucket_object" "airflow_requirements" {
 #   runtime          = "go1.x"
 #   source_code_hash = filebase64sha256("${path.module}/lambda-datasync-dags.zip")
 # }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "datasync-dags.zip"
+  function_name = "datasync-dags"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("datasync-dags.zip")
+
+  runtime = "python3.9"
+}
