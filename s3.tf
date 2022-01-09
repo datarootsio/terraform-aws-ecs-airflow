4c433b62f192ae16f8c6043a75505b159ab87bdf
Milena Angelova <milena.angelova@eldersoss.com>
2022-01-09 19:56:04 +0200

    changed path to the lambda's zip

--- s3.tf
+++ s3.tf
@@ -94,5 +94,5 @@ module "lambda" {
   function_name    = "am-datasync-dags"
   handler          = "lambda_handler"
   runtime          = "go1.x"
-  source_code_hash = filebase64sha256("lambda-datasync-dags.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda-datasync-dags.zip")
 }
