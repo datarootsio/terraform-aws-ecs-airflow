e4ce61753c254aa9122c4c0ab974cf689cc6eb00
Milena Angelova <milena.angelova@eldersoss.com>
2022-01-10 23:56:50 +0200

    canged efs access point

--- efs.tf
+++ efs.tf
@@ -71,15 +71,16 @@ resource "aws_security_group_rule" "ecs_loopback_rule" {
 resource "aws_efs_access_point" "airflow" {
   file_system_id = aws_efs_file_system.airflow-efs.id
   posix_user {
-    gid = 1000
-    uid = 1000
+    gid            = 1001
+    uid            = 5000
+    secondary_gids = 1002
   }
   root_directory {
     path = "/opt/airflow"
     creation_info {
-      owner_gid   = 1000
-      owner_uid   = 1000
-      permissions = 755
+        gid         = 1001
+        uid         = 5000
+        permissions = 0755
     }
   }
   tags = {
