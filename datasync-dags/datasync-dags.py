c394d9b7b89c364469690c71f3c02e0f480424da
Milena  Angelova <milenaangelova@Milenas-MacBook-Pro.local>
2022-04-07 15:17:32 +0300

    cosmetic change

--- datasync-dags/datasync-dags.py
+++ datasync-dags/datasync-dags.py
@@ -1,5 +1,5 @@
 import boto3, os
 
-client = boto3.client('datasync', region_name=os.environment['REGION'])
+client = boto3.client('datasync', region_name=os.environ['REGION'])
 def handler(context, event):
     response = client.start_task_execution(TaskArn=os.environ['TASK_ID'])
